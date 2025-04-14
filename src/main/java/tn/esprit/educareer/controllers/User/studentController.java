305b253a03a50e127347c60038630634f6858fb2
AbdelbadiiTlijani <130941741+AbdelbadiiTlijani@users.noreply.github.com>
2025-04-14 21:19:29 +0100

    Student Interface

--- src/main/java/tn/esprit/educareer/controllers/User/studentController.java
+++ src/main/java/tn/esprit/educareer/controllers/User/studentController.java
@@ -1,4 +1,84 @@
 package tn.esprit.educareer.controllers.User;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
 public class studentController {
+
+    @FXML
+    private Label companyGrowthLabel;
+
+    @FXML
+    private AnchorPane dashboardPane;
+
+    @FXML
+    private Button editProfileButton;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private Label reclamationStatusLabel;
+
+    @FXML
+    private Label totalCompaniesLabel;
+
+    @FXML
+    private Label totalReclamationsLabel;
+
+    @FXML
+    private Label totalUsersLabel;
+
+    @FXML
+    private Label userGrowthLabel;
+
+    @FXML
+    private Label userName;
+
+    @FXML
+    private ImageView userPhoto;
+
+    @FXML
+    private Label userRole;
+
+    @FXML
+    private Button viewCompanyEmployeeButton;
+
+    @FXML
+    private Button viewUserButton;
+
+    @FXML
+    void handleEditProfile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleLogout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleViewCompanyEmployee(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleViewEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleViewReclamation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleViewUser(ActionEvent event) {
+
+    }
+
 }
